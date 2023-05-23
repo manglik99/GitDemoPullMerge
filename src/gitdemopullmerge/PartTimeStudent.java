24dc2ac623cbe693618234728741ee24e3cdff72
Manglik Chokshi <manglik99@gmail.com>
2023-05-23 14:20:41 -0400

    Added a new java class

---
+++ src/gitdemopullmerge/PartTimeStudent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitdemopullmerge;
+
+/**
+ *
+ * @author mangl
+ */
+public class PartTimeStudent {
+    //added a class
+    public static void main(String[] args) {
+        
+    }
+    
+}
